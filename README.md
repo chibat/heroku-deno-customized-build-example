c7a5fe86555e39e4b34d1a4c5b85f3bcfaafcefc
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-04-02 17:47:40 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# heroku-deno-customized-build
+
+This is an example using `heroku_build.ts`(Aleph.js example).
+
+## Buildpack
+
+https://github.com/chibat/heroku-buildpack-deno
+
+
---
+++ heroku_build.ts
@@ -0,0 +1,42 @@
+import { moveSync } from "https://deno.land/std@0.91.0/fs/mod.ts";
+
+const text = await Deno.readTextFile("import_map.json");
+const alephPath = JSON.parse(text).imports["aleph/"];
+
+const buildStatus = await Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "run",
+    "--allow-net=deno.land,esm.sh,cdn.esm.sh",
+    "--allow-read",
+    "--allow-write",
+    "--allow-env",
+    "--allow-run",
+    `${alephPath}cli.ts`,
+    "build",
+  ],
+  stdout: "inherit",
+  stderr: "inherit",
+}).status();
+
+console.log(`Build Exit Code: ${buildStatus.code}`);
+
+const startStatus = await Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "run",
+    "--allow-net=deno.land,esm.sh,cdn.esm.sh",
+    `${alephPath}cli.ts`,
+    "start",
+    "--help",
+  ],
+  stdout: "null",
+  stderr: "inherit",
+}).status();
+
+console.log(`Help Exit Code: ${startStatus.code}`);
+
+//const buildDir = Deno.env.get("HEROKU_BUILD_DIR");
+//if (buildDir) {
+//  moveSync(".aleph", `${buildDir}/.aleph`);
+//}
